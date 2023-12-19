2f5e2c52ebb894f0b5e822e49c9f2ef907e272ce
Satyam kumar <114273646+SatyamKaGithub@users.noreply.github.com>
2023-12-19 20:53:21 +0530

    Primitive can't be break into any other data type

---
+++ primitive.java
@@ -0,0 +1,11 @@
+public class primitive {
+    public static void main(String[] args) {
+//        int rollNo = 54;
+//        char alphabet = 'S';
+//        float Decimal = 34.69f;
+//        double largeDecimalNumber = 2432434.234325522;
+//        long largeNumber = 23434874627682384L;
+//        boolean check = true;
+
+    }
+}
