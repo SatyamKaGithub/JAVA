7599a91528c401232a7aa325266bbd1587e52fd2
Satyam kumar <114273646+SatyamKaGithub@users.noreply.github.com>
2023-12-19 20:52:31 +0530

    How to take Inputs

---
+++ Input.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class Input {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+//        System.out.print("Please enter some input : ");
+//        int rollNo = input.nextInt();
+//        System.out.println("Roll number : " + rollNo);
+//
+//          int a = 10;
+//        System.out.println(a);
+
+//         String name = input.nextLine();
+//        System.out.println(name);
+
+//           float marks = input.nextFloat();
+//        System.out.println(marks);
+
+//           double marks = input.nextDouble();
+//        System.out.println(marks);
+
+
+    }
+}
