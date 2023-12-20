b2e94e75f1bd1cd14c1f3bb7726dceb0ff8e2baa
Satyam kumar <114273646+SatyamKaGithub@users.noreply.github.com>
2023-12-20 20:09:19 +0530

    Loops

---
+++ Basics.java
@@ -0,0 +1,21 @@
+public class Basics {
+    public static void main(String[] args) {
+//        int a = 10;
+//        if (a == 10){
+//            System.out.println("Hello");
+//        }
+
+        // While loop
+//        int count = 1;
+//        while (count != 5){
+//            System.out.println(count);
+//            count++;
+//        }
+
+        // For loop
+        for (int count = 1; count < 5; count++){
+            System.out.println(count);
+        }
+
+    }
+}
