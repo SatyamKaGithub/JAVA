3b018d115c671a554508de88163edb5da98eb0fb
Satyam kumar <114273646+SatyamKaGithub@users.noreply.github.com>
2023-12-23 22:32:44 +0530

    while loop

---
+++ While_Loops.java
@@ -0,0 +1,16 @@
+public class While_Loops {
+    public static void main(String[] args) {
+
+        /*
+        syntax - While loop
+        while (condition){
+        body
+        }
+         */
+        int num = 1;
+        while (num <= 5){
+            System.out.println(num);
+            num +=1;
+        }
+    }
+}
