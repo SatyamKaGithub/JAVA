e71df4a11b5bd6ff07edccd2a06fea9f5fcbae89
Satyam kumar <114273646+SatyamKaGithub@users.noreply.github.com>
2024-01-05 13:32:52 +0530

    Nested Switch

---
+++ NestedSwitch.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+public class NestedSwitch {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int empID = in.nextInt();
+        String department = in.next();
+
+        switch (empID) {
+            case 1 -> System.out.println("Satyam");
+            case 2 -> System.out.println("kumar");
+            case 3 -> {
+                System.out.println("Employee number 3 ");
+                switch (department) {
+                    case "IT" -> System.out.println("IT Department");
+                    case "Management" -> System.out.println("Management Department");
+                    default -> System.out.println("Invalid Input");
+                }
+            }
+            default -> System.out.println("invalid input");
+        }
+    }
+}
