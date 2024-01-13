b2ba7940f55460b4ca73e7eca9b0a0cc4efb5c1f
Satyam kumar <114273646+SatyamKaGithub@users.noreply.github.com>
2024-01-13 16:15:31 +0530

    String Anagram

---
+++ StringAnagram.java
@@ -0,0 +1,15 @@
+var minSteps = function(s, t) {
+    let count = new Array(26).fill(0);
+    for(let i=0; i<s.length; i++){
+        count[s[i].charCodeAt(0) - 'a'.charCodeAt(0)]++;
+        count[t[i].charCodeAt(0) - 'a'.charCodeAt(0)]--;
+    }
+
+    let sum = 0
+    for(let n of count){
+        if(n > 0){
+            sum += n;
+        }
+    }
+    return sum;
+};
